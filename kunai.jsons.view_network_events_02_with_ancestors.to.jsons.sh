3dee63e133ed18341a926f683220bb1d963ac84e
hyde <@hyde>
2025-05-22 11:57:04 +0200

    :::: bin      - new minor content  - kunai.jsons.view_network_events_02_with_ancestors.to.jsons.sh

---
+++ kunai.jsons.view_network_events_02_with_ancestors.to.jsons.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+stdinWarning() {
+  echo "" >&2
+  echo " :: nothing from STDIN. exit." >&2
+  echo "" >&2
+  showExample
+}
+
+showExample() {
+
+  echo "cat /tmp/data.json | $(basename "$0") "
+  echo " "
+}
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
+  echo "NAME"
+  echo -e "  $(basename "$0") - show data as connect view (test 02 ). t\t [STDIN : json lines] - [out : json lines] "
+  echo ""
+  echo "SYNOPSIS"
+  echo "  $(basename "$0") [-h|--help]"
+  echo ""
+  echo "EXAMPLE"
+  echo "  $(showExample)"
+  echo
+  exit 1
+fi
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+if [ -t 0 ]; then
+  stdinWarning
+else
+  (
+    cat - | jq -c '
+    
+      {
+        utc_time:     (.info.utc_time           // .utc_time),
+        event_name:   (.info.event.name         // .event_name),
+        host:         (.info.host.name          // .host          // "?"),
+        dst_ip:       (.data.dst.ip             // .dst           // "?"),
+        dst_port:     (.data.dst.port           // .dst_port      // "?"),
+        dst_fqdn:     (.data.dst.hostname       // .fqdn          // "?"),
+        socket:       (.data.dst.socket         // .socket        // "?"),
+        bin_path:     (.data.exe.path           // .bin_path      // "?"),
+        community_id: (.data.community_id       // .community_id ),
+
+        ancestors: ((.data.ancestors // .ancestors // "null")
+          | split("|")                                     
+          | .[-1])                                            
+      }
+    '
+  )
+fi
